6db0c5409b1039147840689da921a25319339746
Dustin Rath <rathd@unlv.nevada.edu>
2015-09-14 00:40:15 -0700

    Created New Site Links and Pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   def home  	
   end
 
+  def winelist
+  end
+
+  def sommelier
+  end
+
   def about  	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" # Creates about_path
+  get "winelist" => "pages#winelist" # Creates Wine_List_Path
+  get "sommelier" => "pages#sommelier" # Creates Sommelier_Corner_Path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
