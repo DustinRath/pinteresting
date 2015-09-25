75d53b6ac921e055ca693f92fa464eaf17131bb1
Dustin Rath <rathd@unlv.nevada.edu>
2015-09-25 01:35:16 -0700

    Working on pins - Still have errors tho

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,10 +1,7 @@
 class PinsController < ApplicationController
-
-  before_action :authenticate_user!, except: [:index, :show]
-
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
   before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -45,7 +42,7 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find_by(id: params[:id])
+      @pin = Pin.find(params[:id])
     end
 
     def correct_user
